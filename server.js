c77ebede5ecb633f17f900ec7af3c3c85349910c
Gitun34 <142621649+Gitun34@users.noreply.github.com>
2026-04-03 14:31:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>CashClaw is Running! 🚀</h1>');
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`CashClaw running on port ${PORT}`);
-});
+module.exports = (req, res) => {
+  res.status(200).json({
+    status: 'running',
+    message: 'CashClaw is Running! 🚀',
+    version: '1.0.0'
+  });
+};
